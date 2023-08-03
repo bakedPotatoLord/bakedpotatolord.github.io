8fd051a5e21cceb094110198a64f8fea514deef9
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-08-03 16:51:36 -0600

    create curves

---
+++ components/diagonalDivider.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="custom-shape-divider-top-1691101223">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
+    </svg>
+</div>
+</template>
+
+
+<style scoped lang="scss">
+.custom-shape-divider-top-1691101223 {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    overflow: hidden;
+    line-height: 0;
+    transform: scaleY(-1) translateY(-1px);
+    margin-block-end:0px;
+    svg {
+        position: relative;
+        display: block;
+        width: calc(100% + 1.3px);
+        height: 100px;
+    }
+    .shape-fill {
+        fill: #39125c;
+    }
+}
+</style>
