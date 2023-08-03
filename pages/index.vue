466f82ed362581c5049413ad6e650ec12b26d5fe
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-08-03 16:52:33 -0600

    large scale update

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,4 @@
-<template>
-  <div class="maintext">
-    <div class="potatoContainer">
-      <Potato id="potato" />
-    </div>
-    <h1>
-      C<span class="potato">🥔</span>de P<span class="potato">🥔</span>tato
-    </h1>
-    <h2>
-      The <span ref="a" class="adjective">{{ adjective }} </span>
-      <span class="fullstack"> Fullstack</span><br />Web Developer
-    </h2>
-  </div>
-  <div class="about">
-    <h3>About</h3>
-
-    <p>
-      Hi, I'm Josiah Hamm, a {{ age }} year old freelance developer based in
-      Denver, CO, with a passion for software development, leadership,
-      problem-solving, and teamwork.<br />
-      <br />
-      I enjoy building beautiful and functional web applications
-      that join art and technology to create engaging user experiences 
-      
-    </p>
-  </div>
-
-  <div class="headshot">
-    <img src="~/assets/headshot.jpg" alt="headshot" />
-  </div>
-</template>
-
 <script setup lang="ts">
-function rand(arr: any[]) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
 
 const adjectives =[
     "Starchy",
@@ -53,55 +18,143 @@ const ageMs = currentTime - birthTime;
 const age = ref(Math.floor(ageMs / year));
 
 const i = ref(0)
-const adjective = computed(() => {
-  return adjectives[i.value]
-})
-
-
 const a = ref<HTMLSpanElement | null>(null)
+const adjective = computed(() => adjectives[i.value])
 
 onMounted(async ()=>{
-  console.log(a.value)
+  let isOver = false
+  let timeout:ReturnType<typeof setTimeout>;
+
   if(a.value){
     a.value.onmouseover = ()=>{
       console.log('mouse over')
-      i.value = (++i.value) % adjectives.length
+      isOver = true
+      changeWord()
+    }
+    a.value.onmouseleave = ()=>{
+      console.log('mouse leave')
+      isOver = false
+      clearTimeout(timeout)
     }
   }
-})
 
+  function changeWord(){
+    if(isOver) i.value = (++i.value) % adjectives.length
+    timeout = setTimeout(changeWord, 750)
+  }
+})
 </script>
 
+<template>
+  <div class="intro">
+    <div class="maintext">
+      <div class="textContainer">
+        <h1>
+          C<span class="potato">🥔</span>de P<span class="potato">🥔</span>tato
+        </h1>
+        <h2>
+          The <span ref="a" class="adjective">{{ adjective }} </span>
+          <span class="fullstack"> Fullstack</span><br />Web Developer
+        </h2>
+        <p>
+          After an enlightening spiritual journey in Idaho, 
+          The Code Potato found his calling in web development.
+        </p>
+      </div>
+    </div>
+    <div class="potatoContainer">
+      <Potato id="potato" />
+    </div>
+  </div>
+  <DiagonalDivider id="diagonalDivider"/>
+  <div class="about">
+    <div class="aboutText">
+        <h3>About</h3>
+        <p>
+          Hi, I'm Josiah Hamm, a {{ age }} year old freelance developer based in
+          Denver, CO, with a passion for software development, leadership,
+          problem-solving, and teamwork.<br />
+          <br />
+          I build beautiful and functional web applications
+          that join art and technology to create engaging user experiences.<br>
+          <br>
+          I also enjoy competitive robotics, reading up on 
+          Star Wars / Warhammer 40,000 lore, camping, hiking and competitve programming.<br>
+          <br>
+        </p>
+    </div>
+    <div class="headshot">
+      <img src="~/assets/headshot.jpg" alt="headshot" />
+    </div>
+  </div>
+</template>
+
 <style lang="scss">
-.maintext {
-  .potatoContainer{
-    width:calc(100% - 4rem);
+.intro{
+  padding-left:2rem;
+  padding-right:2rem;
+  display:flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  .maintext {
     display:flex;
-    justify-content: right;
-    position: absolute;
-    z-index: -1;
+    align-items: center;
+    
+    .textContainer{
+      display:block;
+      min-width: 400px;
+      max-width: 500px;
+      h1 {
+        font-weight: 600;
+      }
+      h2 {
+        font-weight: 300;
+        .adjective{
+          text-shadow: white 0px 0px 10px ;
+        }
+      }
+    }
   }
-  h1 {
-    font-weight: 600;
+  .potatoContainer{
+    flex:1;
+    display:flex;
+    align-items:center;
+    justify-content: center;
   }
-  h2 {
-    font-weight: 300;
-  }
-  margin-bottom: 80px;
+}
+#diagonalDivider{
+  position: relative;
 }
 .about {
-  h3 {
-    font-weight: 400;
+  margin-top: 0px;
+  padding-left:2rem;
+  padding-right:2rem;
+  display:flex;
+  flex-direction: row;
+  flex-wrap:wrap;
+  background-color:  #39125c;
+  .aboutText {
+    h3 {
+      font-weight: 400;
+      font-size: 1.5rem;
+    }
+    max-width:500px
   }
-}
-.headshot {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 1rem;
-  img {
-    width: 200px;
-    border-radius: 50%;
+  .headshot {
+    flex:3;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem;
+    width: 100%;
+    img {
+      transition: box-shadow 0.3s ease-in-out;
+      width: 250px;
+      border-radius: 50%;
+      &:hover{
+        box-shadow: 0 0 20px 10px rgba(0, 0, 0, 0.3);
+      }
+    }
   }
 }
 </style>
