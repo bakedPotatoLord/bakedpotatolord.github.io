002a5ea378b962383486223ec8efb55931c16e49
= <918780@dpsk12.net>
2023-08-09 17:24:20 -0600

    bro im just giving up at this point

--- components/potato.vue
+++ components/potato.vue
@@ -8,36 +8,59 @@ import { Object3D } from "three";
 //@ts-ignore
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
-function err(): never {
-  throw new Error("Failed to load canvas");
-}
-
 const canvas = ref<HTMLCanvasElement | null>(null);
+const width = ref(0);
+let cw = 0,
+  ch = 0,
+  renderer: THREE.WebGLRenderer;
 
 onMounted(() => {
+  width.value = window.innerWidth;
   let remValue = parseFloat(
     getComputedStyle(document.documentElement).fontSize
   );
-  let cw = Math.min(window.innerWidth - 4 * remValue,500);
-  let ch = cw/Math.SQRT2;
+  cw = Math.min(window.innerWidth - 4 * remValue, 400);
+  ch = cw / Math.SQRT2;
+
+  if (canvas.value) {
+    if (width.value > 800) {
+      canvas.value.hidden = false;
+      setupCanvas(canvas.value);
+    } else {
+      canvas.value.hidden = true;
+    }
+  }
 
   document.addEventListener("resize", () => {
+    console.log("resize");
+    width.value = window.innerWidth;
     remValue = parseFloat(getComputedStyle(document.documentElement).fontSize);
-    cw = Math.min(window.innerWidth - 4 * remValue,500);
-    ch = cw/Math.SQRT2;
-    renderer.setSize(cw, ch);
+    cw = Math.min(window.innerWidth - 4 * remValue, 400);
+    ch = cw / Math.SQRT2;
+    renderer?.setSize(cw, ch);
+    if (canvas.value) {
+      if (width.value > 800) {
+        canvas.value.hidden = false;
+        setupCanvas(canvas.value);
+      } else {
+        canvas.value.hidden = true;
+      }
+    }
   });
+});
 
+function setupCanvas(canvas: HTMLCanvasElement) {
+  console.log("setupCanvas");
   const scene = new THREE.Scene();
   scene.background = null;
   const camera = new THREE.PerspectiveCamera(75, cw / ch, 0.1, 1000);
 
   const renderer = new THREE.WebGLRenderer({
-    canvas: canvas.value ?? err(),
+    canvas,
     alpha: true,
   });
   renderer.setSize(cw, ch);
-  camera.position.z = 3.5;
+  camera.position.z = 2.5;
 
   const light = new THREE.PointLight(0xffffff, 1);
   light.position.set(0, 0, 5);
@@ -61,13 +84,13 @@ onMounted(() => {
     requestAnimationFrame(animate);
     if (potato) {
       potato.rotation.y += 0.01;
-      potato.rotation.x += 0.01;
-      potato.rotation.z += 0.01;
+      //potato.rotation.x += 0.01;
+      //potato.rotation.z += 0.01;
     }
 
     renderer.render(scene, camera);
   }
 
   animate();
-});
+}
 </script>
