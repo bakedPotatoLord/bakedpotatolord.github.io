48e620ae049cb6df5e706f3536813a3454f67ec3
bakedpotatolord <jeffy9745@gmail.com>
2024-02-01 21:50:51 -0700

    formatting

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 
-const adjectives =[
-    "Starchy",
-    "Versatile",
-    "Dependable",
-    "Satisfying",
-    "Wholesome",
-    "Down-To-Earth",
-    "Robust",
-    "Resourceful",
+const adjectives = [
+  "Starchy",
+  "Versatile",
+  "Dependable",
+  "Satisfying",
+  "Wholesome",
+  "Down-To-Earth",
+  "Robust",
+  "Resourceful",
 ]
 
 const year = Date.parse("1971-01-01T00:00:00.000Z");
@@ -21,25 +21,25 @@ const i = ref(0)
 const a = ref<HTMLSpanElement | null>(null)
 const adjective = computed(() => adjectives[i.value])
 
-onMounted(async ()=>{
+onMounted(async () => {
   let isOver = false
-  let timeout:ReturnType<typeof setTimeout>;
+  let timeout: ReturnType<typeof setTimeout>;
 
-  if(a.value){
-    a.value.onmouseover = ()=>{
+  if (a.value) {
+    a.value.onmouseover = () => {
       console.log('mouse over')
       isOver = true
       changeWord()
     }
-    a.value.onmouseleave = ()=>{
+    a.value.onmouseleave = () => {
       console.log('mouse leave')
       isOver = false
       clearTimeout(timeout)
     }
   }
 
-  function changeWord(){
-    if(isOver) i.value = (++i.value) % adjectives.length
+  function changeWord() {
+    if (isOver) i.value = (++i.value) % adjectives.length
     timeout = setTimeout(changeWord, 750)
   }
 })
@@ -62,25 +62,25 @@ onMounted(async ()=>{
       <Potato id="potato" />
     </div>
   </div>
-  <DiagonalDivider id="diagonalDivider"/>
+  <DiagonalDivider id="diagonalDivider" />
   <div class="about">
     <div class="aboutText">
-        <h3>About</h3>
-        <p>
-          Hey there, I'm Josiah Hamm, a freelance developer and robotics engineer based in
-          Denver, CO, with a passion for software development, leadership,
-          problem-solving, and teaching.<br />
-          <br />
-          Some days,I build beautiful and functional web applications
-          that join art and technology to create engaging user experiences.<br>
-          <br>
-          Other days, I design, test, fabricate and build robots and other
-          machines.<br>
-      
-          I also enjoy competitive robotics, reading up on 
-          Star Wars / Warhammer 40,000 lore, camping, hiking and competitve programming.<br>
-          <br>
-        </p>
+      <h3>About</h3>
+      <p>
+        Hey there, I'm Josiah Hamm, a freelance developer and robotics engineer based in
+        Denver, CO, with a passion for making ideas real, leadership,
+        problem-solving, and teaching.<br />
+        <br />
+        Some days, I build beautiful and functional web applications
+        that join art and technology to create engaging user experiences.<br>
+        <br>
+        Other days, I design, test, and fabricate robots and other
+        machines. I love watching something move from a thought to a digital model to reality<br>
+        <br>
+        I also enjoy competitive robotics, reading up on
+        Star Wars / Warhammer 40,000 lore, camping, hiking and competitve programming.<br>
+        <br>
+      </p>
     </div>
     <div class="headshot">
       <img src="~/assets/headshot.jpg" alt="headshot" />
@@ -89,71 +89,83 @@ onMounted(async ()=>{
 </template>
 
 <style lang="scss">
-.intro{
-  padding-left:2rem;
-  padding-right:2rem;
-  display:flex;
+.intro {
+  padding-left: 2rem;
+  padding-right: 2rem;
+  display: flex;
   flex-direction: row;
   flex-wrap: wrap;
+
   .maintext {
-    display:flex;
+    display: flex;
     align-items: center;
-    .textContainer{
-      display:block;
+
+    .textContainer {
+      display: block;
       min-width: 400px;
       max-width: 500px;
+
       h1 {
         font-weight: 600;
       }
+
       h2 {
         font-weight: 300;
         margin-bottom: 0px;
-        .adjective{
-          text-shadow: white 0px 0px 10px ;
+
+        .adjective {
+          text-shadow: white 0px 0px 10px;
         }
       }
     }
   }
-  .potatoContainer{
-    flex:1;
-    display:flex;
-    align-items:center;
+
+  .potatoContainer {
+    flex: 1;
+    display: flex;
+    align-items: center;
     justify-content: center;
   }
 }
-#diagonalDivider{
+
+#diagonalDivider {
   position: relative;
 }
+
 .about {
   margin-top: 0px;
-  padding-left:2rem;
-  padding-right:2rem;
-  display:flex;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  display: flex;
   flex-direction: row;
-  flex-wrap:wrap;
-  background-color:  #39125c;
+  flex-wrap: wrap;
+  background-color: #39125c;
+
   .aboutText {
     h3 {
       font-weight: 400;
       font-size: 1.5rem;
     }
+
     max-width:500px
   }
+
   .headshot {
-    flex:3;
+    flex: 3;
     display: flex;
     justify-content: center;
     align-items: center;
     padding: 1rem;
     width: 100%;
+
     img {
       transition: box-shadow 0.3s ease-in-out;
       width: 250px;
       border-radius: 50%;
-      &:hover{
+
+      &:hover {
         box-shadow: 0 0 20px 10px rgba(0, 0, 0, 0.3);
       }
     }
   }
-}
-</style>
+}</style>
