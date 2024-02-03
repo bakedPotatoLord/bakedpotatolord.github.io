09ec4576ddd0307432b70d14d70138d400108a1f
bakedpotatolord <jeffy9745@gmail.com>
2024-02-02 18:23:44 -0700

    make shape divider for the about page

---
+++ components/dividers/AboutDivider1.vue
@@ -0,0 +1,39 @@
+
+<template>
+  <div class="wrapper">
+    <div class="custom-shape-divider-bottom-1706922823">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M0,0V6c0,21.6,291,111.46,741,110.26,445.39,3.6,459-88.3,459-110.26V0Z" class="shape-fill"></path>
+        </svg>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+.wrapper{
+  position: relative;
+}
+
+.custom-shape-divider-bottom-1706922823 {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    overflow: hidden;
+    line-height: 0;
+    transform: rotate(180deg);
+}
+
+.custom-shape-divider-bottom-1706922823 svg {
+    position: relative;
+    display: block;
+    width: calc(160% + 1.3px);
+    height: 65px;
+}
+
+.custom-shape-divider-bottom-1706922823 .shape-fill {
+    fill: #39125c;
+}
+</style>
