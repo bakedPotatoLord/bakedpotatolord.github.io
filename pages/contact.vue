8b5e74a5c74245c11000ab982892c144d7b3687c
bakedpotatolord <jeffy9745@gmail.com>
2024-07-04 12:55:48 -0600

    delete contact and portfolio pages

--- pages/contact.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <h2>
-    this is the contact page
-  </h2>
-</template>
--- pages/portfolio.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="base">
-    <h2>this is the portfolio</h2>
-  </div>
-</template>
-
-<style lang="scss" scoped>
-  
-</style>
