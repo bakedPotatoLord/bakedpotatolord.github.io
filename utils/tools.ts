545892ca8cac38d4e29be14776b0b835c04d04a3
bakedpotatolord <jeffy9745@gmail.com>
2024-07-04 13:39:28 -0600

    first pass at tools and URLs

---
+++ utils/tools.ts
@@ -0,0 +1,29 @@
+
+export enum tool {
+  Vue,
+  Nuxt,
+  TypeScript,
+  Vite,
+  Bun,
+  Java,
+  Onshape,
+  Vcarve,
+  WPILib,
+
+}
+
+const toolMap = new Map<tool, string>([
+  [tool.Nuxt, "/images/icons/nuxt-icon.svg"],
+  [tool.Vue, "/images/icons/vue.webp"],
+  [tool.TypeScript, "/images/icons/typescript.png"],
+  [tool.Vite, "/images/icons/vite.svg"],
+  [tool.Bun, "/images/icons/bun.png"],
+  [tool.Java, "/images/icons/java.png"],
+  [tool.Onshape, "/images/icons/onshape-logo.svg"],
+  [tool.Vcarve, "/images/icons/vcarve.svg"],
+  [tool.WPILib, "/images/icons/wpi.png"],
+])
+
+export const getToolUrl = (tool: tool) => {
+  return toolMap.get(tool) 
+}
