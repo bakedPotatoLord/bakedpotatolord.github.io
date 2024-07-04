d34520832fbfa60276ea36204d662f8a17d7b39b
bakedpotatolord <jeffy9745@gmail.com>
2024-07-04 13:04:09 -0600

    create triangle divider

---
+++ components/dividers/TriangleDivider.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="rel">
+    <div class="custom-shape-divider-top-1720112943">
+      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M1200 0L0 0 598.97 114.72 1200 0z" class="shape-fill"></path>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<style>
+.rel {
+  position: relative;
+}
+
+.custom-shape-divider-top-1720112943 {
+  position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    overflow: hidden;
+    line-height: 0;
+    transform: scaleY(1) translateY(-1px);
+    margin-block-end:0px;
+    svg {
+        position: relative;
+        display: block;
+        width: calc(100% + 1.3px);
+        height: 100px;
+    }
+    .shape-fill {
+        fill: #39125c;
+    }
+}
+</style>
