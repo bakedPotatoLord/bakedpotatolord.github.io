724d2df9d3c3a17cd647df967d2929af532ea753
bakedpotatolord <jeffy9745@gmail.com>
2024-07-05 18:25:07 -0600

    first pass at a blog card

---
+++ components/BlogCard.vue
@@ -0,0 +1,28 @@
+
+<script setup lang="ts">
+
+
+interface BlogCard {
+  title:string
+  images: string[]
+  body?: string
+}
+
+defineProps<BlogCard>()
+</script>
+
+<template>
+  <div class="card">
+    <NuxtImg :src="images.at(0)"/>
+    <h2>{{ title }}</h2>
+
+  </div>
+</template>
+
+<style scoped lang="scss">
+.card{
+  background-color: black;
+  border-radius: 2rem;
+  padding:5%;
+}
+</style>
