7c1e45eb11ea2d7aeea64d34d3826d5822f299b5
bakedpotatolord <jeffy9745@gmail.com>
2024-07-07 09:56:54 -0600

    first pass

---
+++ components/ContactCard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+interface ContactCard {
+  socialImage: string
+  tag: string
+  link: string
+}
+
+
+defineProps<ContactCard>()
+
+</script>
+
+<template>
+  <a :href="link">
+    <div class="card">
+      <div class="image">
+        <NuxtImg :src="socialImage" />
+      </div>
+      <div class="tag">
+        <h3>{{ tag }}</h3>
+      </div>
+    </div>
+  </a>
+</template>
