93454008432f7a3345bdf619e807bb7df071820e
bakedpotatolord <jeffy9745@gmail.com>
2024-07-14 22:03:12 -0600

    add blog see more button

--- components/sections/Blog.vue
+++ components/sections/Blog.vue
@@ -14,13 +14,16 @@ const { data: content, status } = useAsyncData(async () =>
     <div class="blogItems" v-if="status === 'success'">
       <div v-for="post of content" class="blogItem">
         <a :href="'/blog/' + post.titleUrl">
-            <BlogCard 
-            :title="post.title"
-            :image="post.coverImg"
-            :imageAlt="post.coverImgAlt"
-            :description="post.description"
-            />
+          <BlogCard :title="post.title" :image="post.coverImg" :imageAlt="post.coverImgAlt"
+            :description="post.description" />
         </a>
+      </div>
+    </div>
+    <div class="container">
+      <div class="seeMore">
+        <RouterLink to="/blog">
+          See more
+        </RouterLink>
       </div>
     </div>
   </div>
@@ -42,8 +45,25 @@ const { data: content, status } = useAsyncData(async () =>
     flex-wrap: wrap;
     justify-content: space-evenly;
     align-items: center;
-    padding-bottom: 5rem;
   }
-  
+
+  .container {
+    text-align: center;
+    display: flex;
+    justify-content: center;
+
+
+    padding-bottom: 5rem;
+
+    .seeMore {
+      background-color: rgb(64, 61, 64);
+      border-radius: 10px;
+      padding: 1rem;
+      margin:1rem;
+      width:fit-content;
+
+    }
+  }
+
 }
 </style>
