444a42267f6911c07a675923ecfe30ced8aef41f
bakedpotatolord <jeffy9745@gmail.com>
2024-07-14 21:41:50 -0600

    first pass at blog page

---
+++ pages/blog/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type { QueryBuilderParams } from '@nuxt/content';
+
+const query: QueryBuilderParams = {
+  where: [{ inProgress: { $ne: "true" } }],
+  sort: [{ datePublished: -1 }]
+}
+
+</script>
+
+
+<template>
+  <SectionTitle title="Potato Blog"/>
+  <ContentList :query="query" v-slot="{list}" class="ContentList">
+    <div class="content" v-for="post of list" :key="post._path">
+      <a :href="'/blog/' + post.titleUrl">
+        <BlogCard 
+        :title="post.title"
+        :image="post.coverImg"
+        :imageAlt="post.coverImgAlt"
+        :description="post.description"
+        />
+      </a>
+    </div>
+  </ContentList>
+
+</template>
