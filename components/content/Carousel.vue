2ed049278473d74768a989ed76c8bebff026fefc
bakedpotatolord <jeffy9745@gmail.com>
2024-08-16 21:33:03 -0600

    make carousel

---
+++ components/content/Carousel.vue
@@ -0,0 +1,34 @@
+<template>
+  {{  }}
+  <div class="carousel">
+    <div class="image" v-for="image of images">
+      <NuxtImg  :src="image"  />
+    </div> 
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{images: string[]}>()
+</script>
+
+<style scoped lang="scss">
+.carousel {
+  padding-top: 2rem;
+  padding-bottom: 2rem;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  overflow-x: scroll;
+  scroll-snap-type: x mandatory;
+  scroll-behavior: smooth;
+
+  .image {
+    scroll-snap-align: start;
+    width: 100%;
+    flex: 2 0 400px;
+    margin-right: 2rem;
+  }
+}
+</style>
+
+
