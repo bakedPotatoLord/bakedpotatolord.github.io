c3272c6f918809b44378b96442e9bf855e1ecb37
bakedpotatolord <jeffy9745@gmail.com>
2024-08-18 19:15:34 -0600

    fix styling

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -12,18 +12,39 @@ const query: QueryBuilderParams = {
 
 
 <template>
-  <SectionTitle title="Potato Blog"/>
-  <ContentList :query="query" v-slot="{list}" class="ContentList">
-    <div class="content" v-for="post of list" :key="post._path">
-      <a :href="'/blog/' + post.titleUrl">
-        <BlogCard 
-        :title="post.title"
-        :image="post.coverImg"
-        :imageAlt="post.coverImgAlt"
-        :description="post.description"
-        />
-      </a>
-    </div>
-  </ContentList>
+  <SectionTitle title="Potato Blog" />
+  <div class="contentList">
+    <ContentList :query="query" v-slot="{list}" >
+      <div class="content" v-for="post of list" :key="post._path">
+        <a :href="'/blog/' + post.titleUrl">
+          <BlogCard 
+          :title="post.title"
+          :image="post.coverImg"
+          :imageAlt="post.coverImgAlt"
+          :description="post.description"
+          />
+        </a>
+      </div>
+    </ContentList>
+  </div>
 
 </template>
+
+<style scoped lang="scss">
+
+.contentList {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 1rem;
+  justify-content: center;
+
+  .content{
+    color:pink;
+    a{
+      width:fit-content
+    }
+  }
+}
+
+</style>
