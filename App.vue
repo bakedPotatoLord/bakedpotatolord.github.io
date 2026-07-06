19a190ef5fe503a62edd4b8ae9189893db7783f8
bakedpotatolord <jeffy9745@gmail.com>
2026-07-05 19:55:52 -0600

    move global styles to their own file

--- App.vue
+++ App.vue
@@ -11,6 +11,7 @@
 </template>
 
 <script setup lang="ts">
+import "./global.css";
 
 useServerSeoMeta({
   title: 'BakedPotatoLord | Josiah Hamm',
@@ -39,36 +40,5 @@ useHead({
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100;200;300;400;500;600;700;800&display=swap');
 
-.background {
-  background: none;
-  .divider {
-    position: relative;
-  }
-
-  .pagewrapper {
-    padding-top:3rem;
-  }
-}
-
-
-body {
-  background: rgb(27, 13, 37);
-  background: linear-gradient(-45deg,
-      rgb(27, 13, 37) 0%,
-      rgba(27, 13, 37  , 1) 100%);
-  margin: 0px;
-  font-family: 'Noto Sans', sans-serif;
-  font-weight: 300;
-  color: #fff;
-}
-
-.content{
-  line-height: 180%;
-}
-
-a:-webkit-any-link{
-  color:white;
-  text-decoration: none;
-}
-
 </style>
+
