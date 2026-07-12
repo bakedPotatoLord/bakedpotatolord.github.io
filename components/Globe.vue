8cc28e16e6faeea7f320a8965c380518bbef70a5
bakedpotatolord <jeffy9745@gmail.com>
2026-07-11 21:14:17 -0600

    format new component

--- components/Globe.vue
+++ components/Globe.vue
@@ -2,20 +2,17 @@
 import allShaders from "~/utils/shaders"
 
 const globeShader = allShaders.Sphere;
-
 const canvas = ref<HTMLCanvasElement | null>(null)
-
 const runShader = ref(true)
-
-let gl: WebGL2RenderingContext|undefined|null;
-
 const router = useRouter()
 
-onMounted( () => {
-  console.log(canvas)
-  gl = canvas.value?.getContext("webgl2",{ antialias: true })
+let gl: WebGL2RenderingContext | undefined | null;
 
-  if(!gl || !canvas.value){
+onMounted(() => {
+  console.log(canvas)
+  gl = canvas.value?.getContext("webgl2", { antialias: true })
+
+  if (!gl || !canvas.value) {
     console.error("no gl context")
     return
   }
@@ -25,57 +22,41 @@ onMounted( () => {
   gl.canvas.height = width;
 
   window.addEventListener("resize", resizeCanvas)
-  
-  shaderSwitch()
+  globeShader.shaderSetup(gl)
   mainLoop()
 })
 
-onUnmounted(()=>{
+onUnmounted(() => {
   window.removeEventListener("resize", resizeCanvas)
 })
-function shaderSwitch(){
-  if(gl){
-    //call new shader setup
-    globeShader.shaderSetup(gl)
-    //reset uniforms
-
-  }
-}
 
 function resizeCanvas() {
-  if(canvas.value){
+  if (canvas.value) {
     canvas.value.width = window.innerWidth * 0.9
     canvas.value.height = window.innerWidth * 0.9
   }
-  if(gl){
-    gl.viewport(0,0,canvas.value?.width ?? 0,canvas.value?.height ?? 0)
+  if (gl) {
+    gl.viewport(0, 0, canvas.value?.width ?? 0, canvas.value?.height ?? 0)
   }
 }
 
-
 function mainLoop() {
   globeShader.shaderLoop()
-
-  if(runShader.value) requestAnimationFrame(mainLoop);
+  if (runShader.value) requestAnimationFrame(mainLoop);
 }
-
 </script>
 
 <template>
-  <canvas ref="canvas" class="globeCanvas" @click="router.push('/shaders')">
-</canvas>
-
+  <canvas ref="canvas" class="globeCanvas" @click="router.push('/shaders')"
+    title="Stop hovering and just click! I promise I'll make it worth your while...">
+  </canvas>
 </template>
 
 <style scoped>
-
-.globeCanvas{
-  z-index:  1;
-  position:fixed;
-  right:0px;
-  bottom:0px;
-
+.globeCanvas {
+  z-index: 1;
+  position: fixed;
+  right: 0px;
+  bottom: 0px;
 }
-
-
 </style>
