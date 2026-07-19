df8c23a83e01baba798d2b7ede1a9397f0c1eae7
bakedpotatolord <jeffy9745@gmail.com>
2026-07-19 14:46:59 -0600

    add show solution toggle and apply code formatting

--- pages/maze/index.vue
+++ pages/maze/index.vue
@@ -12,15 +12,12 @@ useHead({
   ]
 })
 
-
 let Worker: typeof import("~/utils/maze/workerGenerate?worker").default;
 
-const c= ref<HTMLCanvasElement|null>(null)
-
+const c = ref<HTMLCanvasElement | null>(null)
 const state = ref<string>("")
 const showMazeOptions = ref<boolean>(false)
-const drawGLEnabled = ref<boolean>(false)
-
+const showSolution = ref<boolean>(false)
 const mazeData = ref<StartData>({
   width: 20,
   height: 20,
@@ -37,8 +34,7 @@ const lastMazeData: StartData = {
 const router = useRouter();
 
 let mazeExists = false
-
-let viewPortDims = {width: 0, height: 0}
+let viewPortDims = { width: 0, height: 0 }
 
 onMounted(async () => {
 
@@ -49,78 +45,75 @@ onMounted(async () => {
   viewPortDims.width = dims[0];
   viewPortDims.height = dims[1];
 
-  if(process.client){
+  if (process.client) {
     //avoid making webworker if in SSR mode
-    Worker = (await import( "~/utils/maze/workerGenerate?worker")).default
+    Worker = (await import("~/utils/maze/workerGenerate?worker")).default
   }
 
 });
 
+async function doRealtimeGenerate(mazeData: StartData) {
+  const { width, height, blockSize, shape } = mazeData
+  const work = new Worker();
+  work.postMessage({ width, height, blockSize, shape })
 
+  work.onmessage = (e) => {
+    const { completion, state: workerState, done, lines, solution } = e.data as WorkerResponse
 
-async function doRealtimeGenerate(mazeData:StartData){
-  const {width, height, blockSize, shape} = mazeData
-    const work = new Worker();
-    work.postMessage({width, height, blockSize, shape})
-    
-    work.onmessage = (e) => {
-      const {completion, state:workerState, done, lines, solution} = e.data as WorkerResponse
-      
-      const formattedPercent = (completion*100).toFixed(2)+'%';
-      state.value = (workerState==='RDFS')?('RDFSing: '+formattedPercent):('drawing: '+formattedPercent)
-      if(done ){
-        if( !(c.value && lines)) return;
-        work.terminate()
-        mazeExists = true
-        showMazeOptions.value = true
+    const formattedPercent = (completion * 100).toFixed(2) + '%';
+    state.value = (workerState === 'RDFS') ? ('RDFSing: ' + formattedPercent) : ('drawing: ' + formattedPercent)
+    if (done) {
+      if (!(c.value && lines)) return;
+      work.terminate()
+      mazeExists = true
+      showMazeOptions.value = true
 
-        state.value = 'done'
-        c.value.width = width*blockSize
-        c.value.height = height*blockSize
-        
-        setupGL(c.value,lines,solution)
-        drawGL()
+      state.value = 'done'
+      c.value.width = width * blockSize
+      c.value.height = height * blockSize
 
-      }else{
-        state.value = `${workerState}: ${formattedPercent}`
-      };
+      setupGL(c.value, lines, solution)
+      drawGL(showSolution.value)
+
+    } else {
+      state.value = `${workerState}: ${formattedPercent}`
     };
-
-    work.onerror = (e) => {
-      console.log(e)
-    }
   };
 
-const handleSubmit = (e: Event) => {
-    e.preventDefault()
-    let data = (new FormData(<HTMLFormElement>e.target))
-    state.value = 'generating ...'
-    requestAnimationFrame(() => {
-      try {
-        doRealtimeGenerate(
-          mazeData.value
-        )
-      } catch (err:any) {
-        if (err[0]) alert(err[1])
-        console.log(err)
-        return
-      }
-    })
+  work.onerror = (e) => {
+    console.log(e)
   }
+};
 
-function validateChange(){
-  let {width, height, blockSize} = mazeData.value
-  if(width*blockSize>viewPortDims.width || height*blockSize>viewPortDims.height){
+const handleSubmit = (e: Event) => {
+  e.preventDefault()
+  let data = (new FormData(<HTMLFormElement>e.target))
+  state.value = 'generating ...'
+  requestAnimationFrame(() => {
+    try {
+      doRealtimeGenerate(
+        mazeData.value
+      )
+    } catch (err: any) {
+      if (err[0]) alert(err[1])
+      console.log(err)
+      return
+    }
+  })
+}
+
+function validateChange() {
+  let { width, height, blockSize } = mazeData.value
+  if (width * blockSize > viewPortDims.width || height * blockSize > viewPortDims.height) {
     alert('maze too big for your renderer')
     mazeData.value = lastMazeData
-  }else{
+  } else {
     lastMazeData.width = width
     lastMazeData.height = height
     lastMazeData.blockSize = blockSize
   }
 }
 
-        
 //form submission button
 //download button
 function downloadMaze(e: Event) {
@@ -139,152 +132,132 @@ function downloadMaze(e: Event) {
 </script>
 
 <template>
-
-  <form @submit="handleSubmit">
+  <form @submit="handleSubmit" class="inputForm">
     <table>
       <tbody>
-      <tr>
-        <th><label for="width">Maze Width (Cells)</label></th>
-        <th><input type="number" name="width"  min="0" v-model="mazeData.width" @input="validateChange"></th>
-      </tr>
+        <tr>
+          <th><label for="width">Maze Width (Cells)</label></th>
+          <th><input type="number" name="width" min="0" v-model="mazeData.width" @input="validateChange"></th>
+        </tr>
       </tbody>
       <tbody>
         <tr>
           <th><label for="heigth">Maze Height (Cells)</label></th>
-          <th><input type="number" name="heigth"  min="0" v-model="mazeData.height" @input="validateChange"></th>
+          <th><input type="number" name="heigth" min="0" v-model="mazeData.height" @input="validateChange"></th>
         </tr>
       </tbody>
       <tbody>
         <tr>
           <th><label for="cellSize">Cell Size</label></th>
-          <th><input type="number" name="cellSize"  min="5" v-model="mazeData.blockSize" @input="validateChange"></th>
+          <th><input type="number" name="cellSize" min="5" v-model="mazeData.blockSize" @input="validateChange"></th>
         </tr>
       </tbody>
     </table>
-
     <div class="options">
-      <a href="./visualize/">Visualize Maze Generation</a><br>
+      <a class="visLink buttonStyle" @click="router.push('/maze/visualize')">Visualize Maze Generation</a><br>
       <label for="shape">Cell Shape</label>
       <select name="shape" id="" v-model="mazeData.shape" @input="validateChange">
         <option value="4" selected>Square</option>
         <option value="6">Hexagon</option>
         <!--  <option value="3">Triangle</option> -->
       </select>
-      <input type="submit" value="Generate Maze">
+      <input  class = "buttonStyle" type="submit" value="Generate Maze">
     </div>
-
   </form>
   <div class="mazeOptions" v-if="showMazeOptions">
     <label for="showSolution">Show Solution</label>
-    <input type="checkbox" name="showSolution" id="showSolution">
+    <input type="checkbox" name="showSolution" id="showSolution" v-model="showSolution" @change="drawGL(showSolution)">
     <button @click="downloadMaze">Download Maze</button>
   </div>
   <div class="stateContainer">
-    <p id="state" >{{ state ?? ""}}</p>
+    <p id="state">{{ state ?? "" }}</p>
   </div>
   <div class="canvases">
     <canvas ref="c"></canvas>
   </div>
 </template>
 
-
-<style>
-
+<style scoped lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
 
-body{
-  margin: 0px;
-  background-color: rgb(38, 38, 38);
-}
-
-*{
+* {
   font-family: 'Open Sans', sans-serif;
 }
 
-
-form{
-  background-color: rgb(81, 73, 83);
+.inputForm {
+  background-color: rgb(64, 61, 64);
   border-radius: 20px;
   text-align: center;
   padding-bottom: 2%;
-  width:75%;
+  width: 75%;
   margin-left: auto;
   margin-right: auto;
   margin-top: 10px;
-}
-
-table{
-  padding: 5%;
-  margin-left: auto;
-  margin-right: auto;
-
-}
-
-table tr th{
-  text-align: left;
-}
-
-label{
-  color: wheat;
-  font-weight: bold;
+  table {
+    padding: 5%;
+    margin-left: auto;
+    margin-right: auto;
+    th {
+      text-align: left;
+      label {
+        font-weight: bold;
+      }
+    }
+  }
 }
 
 input,
-select{
-  border: 2px inset rgb(118, 118, 118);
+select {
+  border: 2px groove rgb(118, 118, 118);
+  border-radius: 0.4rem;
   background-color: grey;
-  color:whitesmoke;
+  color: whitesmoke;
   font-weight: bold;
 }
 
-input[type=submit],
-.mazeOptions button{
-  border: none;
-  padding: 4px;
-  border-radius: 4px;
-  margin:5px;
-  background-color:rgb(35, 37, 41);
-  color:aliceblue;
-  font-weight: bold;
-  transition:ease-in-out;
-  transition-duration: 250ms;
-}
-
-
-form a{
-  color: wheat;
-  text-decoration: underline;
-
-}
-
-.mazeOptions{
-  background-color: rgb(81, 73, 83);
+.mazeOptions {
+  background-color: rgb(64, 61, 64);
   border-radius: 20px;
   text-align: center;
   padding-bottom: 1%;
   padding-top: 1%;
-  width:75%;
+  width: 75%;
   margin-left: auto;
   margin-right: auto;
   margin-top: 10px;
-  color:whitesmoke
+  color: whitesmoke;
+  .visLink {
+   margin-bottom: 10rem;
+  }
 }
 
-.stateContainer{
+input[type=submit],
+.mazeOptions button,
+.visLink {
+  border: none;
+  padding: 4px;
+  border-radius: 4px;
+  margin: 5px;
+  background-color: rgb(35, 37, 41);
+  color: aliceblue;
+  font-weight: bold;
+  transition: ease-in-out;
+  transition-duration: 250ms;
+}
+
+.stateContainer {
   text-align: center;
+  #state {
+    font-size: small;
+  }
 }
 
-#state{
-  color:wheat;
-  font-size: small;
-}
-
-.canvases{
+.canvases {
   text-align: center;
+  canvas {
+    border: none;
+    max-width: 90vw;
+  }
 }
 
-canvas{
-  border:none;
-  max-width: 90vw;
-}
 </style>
