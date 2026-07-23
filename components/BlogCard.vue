e9cbefe02db184bd73ae08b8216aaf875a97f875
bakedpotatolord <jeffy9745@gmail.com>
2026-07-23 17:14:00 -0600

    add blog header

--- components/BlogCard.vue
+++ components/BlogCard.vue
@@ -17,8 +17,9 @@ defineProps<BlogCard>()
 
     <h3 class="title">{{ title }}</h3>
     <NuxtImg :src="image" width="500px" sizes=" sm:200px md:500px lg:700px" :alt="imageAlt" class="image" />
-    <p class="description">{{ description }}</p>
-
+    <div class="descContainer">
+      <p class="description">{{ description }}</p>
+    </div>
   </div>
 </template>
 
@@ -26,32 +27,38 @@ defineProps<BlogCard>()
 .blogItem {
   max-width: 600px;
   min-width: 200px;
+  width:31rem;
   background-color: rgb(64, 61, 64);
   border-radius: 10px;
   padding: 2%;
   margin: 2%;
   aspect-ratio: 1.618 / 1;
-  
+  display: flex;
+  flex-direction: column;
+  align-items: center ;
+  box-sizing: border-box;
 
+  
   .title {
     margin-top: 0.1ch;
     font: 400 1.5rem;
     height: 4ch;
   }
-
   .image {
     max-width: 100%;
     border-radius: 1rem;
     aspect-ratio: 1.618 /1;
   }
-
-  .description {
-    margin-bottom: 0px;
+  .descContainer {
+    height: 6ch;
     overflow: hidden;
-    height: 5ch
-
+    margin-top: 0.5rem;
+    width:100%;
+    .description {
+      margin: 0px;
+      min-width: 0;
+    }
   }
-
   &:hover {
     box-shadow: 0 0 10px 10px rgba(64, 61, 64, 0.5);
   }
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -12,7 +12,14 @@ const query: QueryBuilderParams = {
 
 
 <template>
-  <SectionTitle title="Potato Blog" />
+  <div class="header">
+    <SectionTitle title="Potato Blog" justify="center" />
+    <p class="desc">
+      A collection of blog posts by Josiah Hamm. I try to maintain a variety of topics, 
+      and always include lots of images when possible. Please don't 
+      republish or reproduce any of my content without my permission.
+    </p>
+  </div>
   <div class="contentList">
     <ContentList :query="query" v-slot="{list}" >
       <div class="content" v-for="post of list" :key="post._path">
@@ -27,11 +34,21 @@ const query: QueryBuilderParams = {
       </div>
     </ContentList>
   </div>
-
+ 
 </template>
 
 <style scoped lang="scss">
-
+.header{
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  .desc{
+    max-width: 600px;
+    width: 50%;
+  }
+}
 .contentList {
   display: flex;
   flex-direction: row;
@@ -42,7 +59,7 @@ const query: QueryBuilderParams = {
   .content{
     color:pink;
     a{
-      width:fit-content
+      display: block;
     }
   }
 }
