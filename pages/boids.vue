836dc63995c82bcdd0f196ffc4c805e75fb0ca51
bakedpotatolord <jeffy9745@gmail.com>
2026-07-25 21:56:18 -0600

    add boids skeleton

---
+++ pages/boids.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+
+useHead({
+})
+
+useSeoMeta({
+})
+
+
+const c = ref<HTMLCanvasElement | null>(null)
+
+
+onMounted(async () => {
+  if (!c.value) throw new Error("no canvas")
+
+});
+
+onUnmounted(() => {
+})
+
+</script>
+
+<template>
+  <div class="title">
+    <SectionTitle title="Boids" justify="left"/>
+  </div>
+  <div class="container">
+    <div class="canvases">
+      <canvas ref="c"></canvas>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+
+</style>
